eb6416de1df5bea34ac47dd6c24841e8a8736021
jesatuts2 <jessica.hyne@uts.edu.au>
2024-07-23 15:26:39 +0100

    added methods for gathering all meet info for all teams

--- Notebooks/gymternet_1.py
+++ Notebooks/gymternet_1.py
@@ -29,7 +29,7 @@ def get_url_and_wait_for_elements_to_load(url, css_selector):
         print(element)
     except:
         print("oh no it didn't work")
-        exit()
+        pass # ?? Trying to make it so that the program doesn't crash if the element isn't found
     
 
 get_url_and_wait_for_elements_to_load(url, 'div.rt-table > div.rt-tbody')
@@ -75,17 +75,17 @@ def get_team_info(year, url):
 
 # ******************************************************************************************************************************
 # 3 Paginate through the 'years' drop down menu and select the next year in the list and call step 2 again
-def paginate_through_meets_over_the_years(years, url):
+def paginate_through_teams_over_the_years(years, url):
     for year in years:
         # Load the team pages and scrape the information
         new_team_url = url + 'final/' + str(year)
-        print(new_url)
+        print(new_team_url)
 
         get_url_and_wait_for_elements_to_load(new_team_url, css_selector)
         response = Selector(text=driver.page_source)
         get_team_info(year, new_team_url)
 
-paginate_through_meets_over_the_years(years, url)
+paginate_through_teams_over_the_years(years, url)
 teams_df = pd.DataFrame({'team_id': team_ids, 'team_name': team_names, 'division': team_divisions, 'link': team_links})
 print(teams_df)
 
@@ -128,3 +128,36 @@ def get_meet_info(url):
 print(team_link)
 # get_url_and_wait_for_elements_to_load(team_link, css_selector)
 get_meet_info(team_link)
+
+# ******************************************************************************************************************************
+# 5 Paginate through every year on a team's page and pick up the meet data
+def paginate_through_years_to_get_meet_data(years, url):
+    for year in years:
+
+        # Load the team pages and scrape the information
+        split_url = url.split('/') # Splitting the url by the '/' character
+        split_url[-2] = str(year) # Changing the year in the url to the current year
+        new_url = '/'.join(split_url) # Joining the url back together
+        print(new_url)
+
+        get_meet_info(new_url)
+
+#         get_url_and_wait_for_elements_to_load(new_url, css_selector)
+#         response = Selector(text=driver.page_source)
+#         get_meet_info(year, new_url)
+
+# ******************************************************************************************************************************
+# 6 Paginate through every year for every team and pick up the meet data
+def paginate_through_teams_to_get_meet_data(years, team_links):
+    for link in team_links:
+        paginate_through_years_to_get_meet_data(years, link)
+
+#         get_url_and_wait_for_elements_to_load(new_url, css_selector)
+#         response = Selector(text=driver.page_source)
+#         get_meet_info(year, new_url)
+
+
+paginate_through_teams_to_get_meet_data(years, team_links)
+meets_df = pd.DataFrame({'meet_id': meet_ids, 'meet_date': meet_dates, 'season': meet_seasons, 'link': meet_links})
+print(meets_df)
+
