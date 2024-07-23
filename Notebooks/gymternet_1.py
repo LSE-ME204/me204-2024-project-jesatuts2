68be7ade3d2d71c39bc5f73e96c13b5d2df87eee
jesatuts2 <jessica.hyne@uts.edu.au>
2024-07-23 14:15:45 +0100

    Added the process of scraping the first meets page

--- Notebooks/gymternet_1.py
+++ Notebooks/gymternet_1.py
@@ -19,19 +19,20 @@ print('honk')
 # ******************************************************************************************************************************
 # PART 1: Go to the url, wait until everything on the page loads
 
-def get_url_and_wait_for_elements_to_load(url):
+def get_url_and_wait_for_elements_to_load(url, css_selector):
     try:
         driver.get(url)
         print("*****************************")
         element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, 'div.rt-table > div.rt-tbody')) # (Source: https://selenium-python.readthedocs.io/waits.html )
+        EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)) # (Source: https://selenium-python.readthedocs.io/waits.html )
         )
         print(element)
     except:
         print("oh no it didn't work")
         exit()
+    
 
-get_url_and_wait_for_elements_to_load(url)
+get_url_and_wait_for_elements_to_load(url, 'div.rt-table > div.rt-tbody')
 response = Selector(text=driver.page_source)
 
 print(response)
@@ -41,17 +42,20 @@ print(response)
 team_names = []
 team_divisions = []
 team_links = []
-table = response.css('div.rt-table > div.rt-tbody')
+team_ids = []
+css_selector = 'div.rt-table > div.rt-tbody'
+table = response.css(css_selector)
 rows = table.css('.rt-tr-group')
 
 def get_team_info(year, url):  
-
     #first_one = rows[1].css('div a::text')
     for row in rows:
         a_selector = row.css('a').get()
-        link = row.css('a::attr(href)').get()
+        link_slug = row.css('a::attr(href)').get()
+        link = 'https://roadtonationals.com' + link_slug
         team_name = row.css('a::text').get()
         division = row.css('div > div:nth-child(8)::text').get()
+        team_id = link_slug.split('/')[-1]
     
         #print(a_selector)
         #print(a_text)
@@ -62,23 +66,65 @@ def get_team_info(year, url):
         elif str(division) != '1': 
             continue
         else:
+            team_ids.append(team_id)
             team_names.append(team_name)
             team_links.append(link)
             team_divisions.append(division)
 
-    print(team_divisions)
-    return team_names, team_divisions, team_links #I'm not sure about this return statement
+    return team_ids, team_names, team_divisions, team_links #I'm not sure about this return statement
 
-# 3 Click on the 'years' drop down menu and select the next year in the list (but we have determined we don't have to do this, but this is how I would instruct a person, not a machine)
-def paginate_through_years(years, url):
+# ******************************************************************************************************************************
+# 3 Paginate through the 'years' drop down menu and select the next year in the list and call step 2 again
+def paginate_through_meets_over_the_years(years, url):
     for year in years:
-        new_url = url + 'final/' + str(year)
+        # Load the team pages and scrape the information
+        new_team_url = url + 'final/' + str(year)
         print(new_url)
-        get_url_and_wait_for_elements_to_load(new_url)
-        get_team_info(year, new_url)
 
-paginate_through_years(years, url)
-teams_df = pd.DataFrame({'team_name': team_names, 'division': team_divisions, 'link': team_links})
+        get_url_and_wait_for_elements_to_load(new_team_url, css_selector)
+        response = Selector(text=driver.page_source)
+        get_team_info(year, new_team_url)
 
-# 4 Do step 2 again
-# 5 Do step 3 again (etc until end of the years list)
+paginate_through_meets_over_the_years(years, url)
+teams_df = pd.DataFrame({'team_id': team_ids, 'team_name': team_names, 'division': team_divisions, 'link': team_links})
+print(teams_df)
+
+# ******************************************************************************************************************************
+# 4 Go to each of the team's links and scrape the meet information
+
+team_link = team_links[1]
+meet_dates = []
+meet_links = []
+meet_ids = []
+meet_seasons = []
+
+
+def get_meet_info(url):
+    css_selector = 'div.rt-tbody'
+    get_url_and_wait_for_elements_to_load(url, css_selector)
+    response = Selector(text=driver.page_source)  
+    
+    table = response.css(css_selector)
+    rows = table.css('div.rt-tbody > div.rt-tr-group')
+    
+    for row in rows:
+        team_id = url.split('/')[-1] # To match to the team_id column in the teams table as a foreign key
+        link_slug = row.css('a::attr(href)').get()
+        link = 'https://roadtonationals.com' + link_slug
+        meet_id = link_slug.split('/')[-1] # To act as the primary key for the meets table
+        meet_date = table.css('div > div > div:nth-child(2)::text').get() #TODO: Convert to datetime
+        season = meet_date.split('-')[-1]
+    
+        if meet_id in meet_ids:  
+            continue
+        else:
+            meet_ids.append(meet_id)
+            meet_dates.append(meet_date)
+            meet_seasons.append(season)
+            meet_links.append(link)
+    print(meet_links)
+    return meet_ids, meet_dates, meet_seasons, meet_links #I'm not sure about this return statement
+
+print(team_link)
+# get_url_and_wait_for_elements_to_load(team_link, css_selector)
+get_meet_info(team_link)
